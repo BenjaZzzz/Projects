6c7f56a83abb6d4cc5dd6da72dd524a6a466e72c
Felix Benjamin Ogando <124589914+BenjaZzzz@users.noreply.github.com>
2025-01-05 01:37:54 -0400

    Add files via upload

---
+++ ZonaFit/DB/zona_fit_db.sql
@@ -0,0 +1,7 @@
+-- CRUD - CREATE(INSERT) - READ(SELECT) - UPDATE(UPDATE) - DELETE
+-- select * from zona_fit_db.cliente;
+-- INSERT INTO `zona_fit_db`.`cliente` (`nombre`, `apellido`, `membresia`) VALUES ('Gabriel ', 'Flores', '100');
+-- UPDATE `zona_fit_db`.`cliente` set `apellido` = 'Ogando', `membresia` = '200' WHERE (`id` = '2');
+-- select * from zona_fit_db.cliente;
+insert into `zona_fit_db`.`cliente` set (`nombre`, `apellido`,`membresia`) values 
+
